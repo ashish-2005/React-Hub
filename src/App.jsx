2a6010447ddd8136ae7be7ca2c5f78d2668525cc
ashishchaubey <ashish.23b0121172@abes.ac.in>
2025-11-04 09:21:51 +0530

    all commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ 
+import './App.css'
+import Accordian from './components/accodian'
+import RandomColor from './components/random-color'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <Accordian /> */}
+      <RandomColor />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/star-rating/index.jsx
@@ -0,0 +1,9 @@
+
+
+
+export default function StarRating(){
+
+    return <div>
+        
+    </div>
+}
